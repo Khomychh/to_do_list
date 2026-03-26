e2a698e7ca72e3b4b503ad6ea8898831d5d31142
Ivan Khomych <ivan.khomychh@gmail.com>
2026-03-26 11:29:25 +0200

    set up database with Alembic, create `tasks` table, and define `Task` model

---
+++ tasks/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    is_completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    due_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
